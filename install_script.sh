8d2aabb0ec3fcde413eaaf90a9a8e28568bb310d
T-O-R-U-S <bageliq@protonmail.com>
2021-05-08 10:36:47 +0400

    Made some changes to the install script

--- install_script.sh
+++ install_script.sh
@@ -1,4 +1,8 @@
-git clone https://github.com/T-O-R-U-S/printfile.git
+echo 'Cloning repo...'
+git clone https://github.com/T-O-R-U-S/printfile.git -q
 cd printfile
+echo 'Compiling binary...'
 rustc src/main.rs -o pf
+echo 'Installing binary...'
 sudo mv pf /usr/local/bin/pf
+echo 'Done!'
